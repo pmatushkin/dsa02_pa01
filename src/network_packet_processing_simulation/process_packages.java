520adf9d38b94ce26c87c1b20dabdd2c1eecec1f
pmatushkin <pmatushkin@gmail.com>
2016-07-24 16:14:12 -0400

    feat: Try Advanced Problem 3: Network packet processing simulation
    Failed case #22/22: time limit exceeded (9.46/6.00)

--- src/network_packet_processing_simulation/process_packages.java
+++ src/network_packet_processing_simulation/process_packages.java
@@ -28,9 +28,43 @@ class Buffer {
         this.finish_time_ = new ArrayList<Integer>();
     }
 
+    private int maxFinishTime = 0;
+
     public Response Process(Request request) {
         // write your code here
-        return new Response(false, -1);
+
+        // this handles the wait if the next packet doesn't arrive immediately
+        if (maxFinishTime < request.arrival_time) {
+            maxFinishTime = request.arrival_time;
+        }
+
+        // remove all finish_times lesser or equal than request.arrival_time
+        int i = 0;
+        int countPacketsInBuffer = this.finish_time_.size();
+        while (i < countPacketsInBuffer) {
+            if (finish_time_.get(i) <= request.arrival_time) {
+                finish_time_.remove(i);
+                countPacketsInBuffer--;
+            } else {
+                i++;
+            }
+        }
+
+        // count the number of packets in finish_time_: this is how many packages are currently in the buffer
+        countPacketsInBuffer = this.finish_time_.size();
+
+        if (countPacketsInBuffer < this.size_) {
+            // get the time when the buffer becomes empty
+            int bufferFinishTime = maxFinishTime;
+
+            // add a new package to the buffer
+            maxFinishTime = maxFinishTime + request.process_time;
+            finish_time_.add(maxFinishTime);
+
+            return new Response(false, bufferFinishTime);
+        } else {
+            return new Response(true, -1);
+        }
     }
 
     private int size_;
